e2282e9d36bde26a0160b4a04ac0b71fa9521747
Emma Tipton <etipton94@gmail.com>
2013-05-23 17:52:26 +0000

    added to message file

--- hello.rb
+++ hello.rb
@@ -1,4 +1,4 @@
 name = "Emma"
 <<<<<<< HEAD
-puts "hi there, how are you,"" ++name++!""Im hungry"
+puts "hi there, how are you,"" ++name++!""Did you enjoy your lunch today?"
 >>>>>>> 3059210151da60f234821392ea08fdca1b844c8a
